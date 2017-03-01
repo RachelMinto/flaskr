1aebad91317a39b07b6256a5e194977c5ebd1389
RachelMinto <rachel.j.minto@gmail.com>
2017-03-01 18:59:42 -0500

    Working application.

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -46,7 +46,7 @@ def close_db(error):
         g.sqlite_db.close()
 
 @app.route('/')
-def.show_entries():
+def show_entries():
     db = get_db()
     cur = db.execute('select title, text from entries order by id desc')
     entries = cur.fetchall()
