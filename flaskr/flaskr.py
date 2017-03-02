73af1575979b6a9b665dd20300c00602a4bc569d
RachelMinto <rachel.j.minto@gmail.com>
2017-03-02 07:00:23 -0500

    Ready to add tests.

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -2,8 +2,8 @@
 
 import os
 import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
-
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
 # creates application ad initializes with config
 
 app = Flask(__name__)
